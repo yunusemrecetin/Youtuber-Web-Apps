5e2d22fdb27e32a0bb243e488de3dd78fbde09de
Yunus Emre CETIN <me@yunusemrecetin.com.tr>
2016-08-30 17:59:08 -0700

    Added js, general edits

---
+++ app/js/youtuber.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Secret on 8/30/2016.
+ */
+$(document).ready(function(){
+
+    /**
+     * Tabs
+     */
+    $('.pointing.menu .item').tab({
+        context:'parent'
+    });
+
+});
